4c39fe21ac618addb4179e943fcd491e982d208d
Jan Müller <jan.mueller@novapo.net>
2015-03-31 15:52:41 +0200

    Create admin area

--- main.go
+++ main.go
@@ -1,14 +1,27 @@
 package main
 
 import (
-	"log"
+	"io/ioutil"
 	"net/http"
 	"strconv"
 
+	"github.com/gorilla/mux"
 	"github.com/novapo/go-spa-kit/config"
 )
 
 func main() {
 	conf := config.GetServerConfig()
-	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(conf.Port), http.FileServer(http.Dir(conf.Path))))
+	r := mux.NewRouter()
+	r.Handle("/", http.FileServer(http.Dir(conf.Path)))
+	r.HandleFunc("/admin", AdminHandler)
+	http.ListenAndServe(":"+strconv.Itoa(conf.Port), r)
+}
+
+func AdminHandler(w http.ResponseWriter, req *http.Request) {
+	adminPage, err := ioutil.ReadFile("admin/admin.html")
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	w.Write(adminPage)
 }
