1ee9c563df19646f8c6bc2f93f59d7544f593e35
Philip Graf <mail@philipgraf.de>
2015-04-01 01:44:37 +0200

    Add comments

--- plugins/plugins.go
+++ plugins/plugins.go
@@ -4,15 +4,18 @@ import "github.com/gorilla/mux"
 
 var registeredPlugins []Plugin
 
+// Plugin is the basic interface for all plugins
 type Plugin interface {
 	RegisterRoutes(r *mux.Router)
 	GetName() string
 }
 
+// Register a plugin
 func Register(p Plugin) {
 	registeredPlugins = append(registeredPlugins, p)
 }
 
+// AddPluginRoutes will add all routes from the plugins to the given router
 func AddPluginRoutes(r *mux.Router) {
 	pluginRouter := r.PathPrefix("/plugins").Subrouter()
 	for _, p := range registeredPlugins {
