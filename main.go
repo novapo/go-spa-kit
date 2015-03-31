0ed16d070563cd392846014d82edadf6f00688a8
Jan Müller <jan.mueller@novapo.net>
2015-03-31 19:23:21 +0200

    Use session cookies for admin authentification

--- main.go
+++ main.go
@@ -6,28 +6,59 @@ import (
 	"strconv"
 
 	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
 	"github.com/novapo/go-spa-kit/config"
 )
+
+var store = sessions.NewCookieStore([]byte("very-secure"))
 
 func main() {
 	conf := config.GetServerConfig()
 	r := mux.NewRouter()
 	r.Handle("/", http.FileServer(http.Dir(conf.Path)))
 	r.HandleFunc("/admin", AdminHandler).Methods("GET")
+	r.HandleFunc("/login", LoginHandler)
 	http.ListenAndServe(":"+strconv.Itoa(conf.Port), r)
 }
 
 func AdminHandler(w http.ResponseWriter, req *http.Request) {
 	adminPage, err := ioutil.ReadFile("admin/admin.html")
-	username, password, ok := req.BasicAuth()
-	if !ok || username != config.AdnConf.Login || password != config.AdnConf.Password {
-		w.Header().Add("WWW-Authenticate", "Basic realm='Login alter'")
-		w.WriteHeader(http.StatusUnauthorized)
-		return
-	}
 	if err != nil {
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
-	w.Write(adminPage)
+	session, _ := store.Get(req, "session-name")
+	// Set some session values.
+	if session.Values["foo"] == "bar" && session.Values[42] == 43 {
+		w.Write(adminPage)
+	} else {
+		w.WriteHeader(http.StatusUnauthorized)
+	}
+}
+
+func LoginHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method == "GET" {
+		loginPage, err := ioutil.ReadFile("admin/login.html")
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+		w.Write(loginPage)
+	}
+	if req.Method == "POST" {
+		req.ParseForm()
+
+		login := req.PostForm.Get("login")
+		password := req.PostForm.Get("password")
+
+		if login == config.AdnConf.Login && password == config.AdnConf.Password {
+			session, _ := store.Get(req, "session-name")
+			// Set some session values.
+			session.Values["foo"] = "bar"
+			session.Values[42] = 43
+			// Save it.
+			session.Save(req, w)
+			http.Redirect(w, req, "/admin", 302)
+		}
+	}
 }
